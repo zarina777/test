ad93d65e6c71623a4f8093bae5397ee3d1ffc2d8
zarina777 <uktamovazarinabaratovna@gmail.com>
2025-01-23 10:16:17 +0500

    Persist used

--- src/main.jsx
+++ src/main.jsx
@@ -2,17 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-
+import { PersistGate } from "redux-persist/integration/react";
 import { ThemeProvider } from "@material-tailwind/react";
 import { Provider } from "react-redux";
-import store from "./store/store";
+import { store, persistor } from "./store/store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
+      <PersistGate loading={<div>Loading...</div>} persistor={persistor}>
+        <ThemeProvider>
+          <App />
+        </ThemeProvider>
+      </PersistGate>
     </Provider>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,27 @@
-// src/store.js
 import { configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
 import projectReducer from "./projectsSlice";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "projects",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, projectReducer);
 
 const store = configureStore({
   reducer: {
-    projects: projectReducer, 
+    projects: persistedReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: ["persist/PERSIST", "persist/REHYDRATE"],
+      },
+    }),
 });
 
-export default store;
+const persistor = persistStore(store);
+
+export { store, persistor };
